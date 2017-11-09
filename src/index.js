c0ba2ca1f5dd461dcc465871907428a15ee688e4
A. van der Pluijm <info@urbanlink.nl>
2017-11-09 23:13:33 +0100

    initiate with config-object, load only required backend

--- src/index.js
+++ src/index.js
@@ -1,25 +1,45 @@
+'use strict';
+
 var FeedManager = require('./FeedManager.js');
-var config = require('./config.js');
-var settings = config();
-var extend = require('util')._extend;
-var waterline = require('./backends/waterline');
-var mongoose = require('./backends/mongoose.js');
+// var config = require('./config.js');
+// var settings = config();
+// var extend = require('util')._extend;
+// var waterline = require('./backends/waterline');
+// var mongoose = require('./backends/mongoose.js');
 
-module.exports.FeedManager = new FeedManager(settings);
-module.exports.feedManagerFactory = function(options) {
-	var withSettings = extend({}, settings);
-	options = options || {};
+var config = {};
 
-	for (var key in withSettings) {
-		if (withSettings.hasOwnProperty(key) && options.hasOwnProperty(key)) {
-			withSettings[key] = options[key];
-		}
-	}
-
-	return new FeedManager(withSettings);
+module.exports.FeedManager = function(settings) {
+  config=settings;
+  return new FeedManager(settings);
 };
 
-module.exports.mongoose = mongoose;
-module.exports.settings = settings;
-module.exports.WaterlineBackend = waterline.Backend;
-module.exports.MongooseBackend = mongoose.Backend;
+// module.exports.feedManagerFactory = function(options) {
+// 	var withSettings = extend({}, settings);
+// 	options = options || {};
+//
+// 	for (var key in withSettings) {
+// 		if (withSettings.hasOwnProperty(key) && options.hasOwnProperty(key)) {
+// 			withSettings[key] = options[key];
+// 		}
+// 	}
+//
+// 	return new FeedManager(withSettings);
+// };
+
+module.exports.backend = function() {
+  if (config.backend === 'mongoose') {
+    return require('./backends/mongoose.js').Backend;
+  }
+  if (config.backend === 'waterline') {
+    return require('./backends/waterline').Backend;
+  }
+  if (config.backend === 'sequelize') {
+    return require('./backends/sequelize').Backend;
+  }
+};
+
+module.exports.settings = config;
+
+// module.exports.WaterlineBackend = waterline.Backend;
+// module.exports.MongooseBackend = mongoose.Backend;
